f2bc5dd9f6808fea1256b5f4a9771c61121a9f5e
David Nguyen <89553049+davido-noowin@users.noreply.github.com>
2025-02-10 17:08:17 -0800

    creating room component

---
+++ inventory/src/app/rooms/rooms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  imports: [],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.css'
+})
+export class RoomsComponent {
+  hotel_name = 'Industrial City Grand Hotel';
+  number_of_rooms = 11;   
+}
