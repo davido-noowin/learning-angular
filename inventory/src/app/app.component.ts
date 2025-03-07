fdd817b1d6b8b41853134e9d671d50c445a7aa52
David Nguyen <davido.nguyen1212@gmail.com>
2025-03-06 16:52:13 -0800

    add roomcomponent to the root render

--- inventory/src/app/app.component.ts
+++ inventory/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RoomsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
